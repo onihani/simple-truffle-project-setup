0dbf51379130e1053ba95292d54abdd1fd023b2f
Nathaniel Quansah <n.bongo40@gmail.com>
2025-02-24 12:27:19 +0000

    contract verification

--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,11 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const mnemonicPhrase = fs.readFileSync(".secret").toString().trim();
+const infuraProjectId = fs.readFileSync(".infura").toString().trim();
+const etherScanKey = fs.readFileSync(".etherscan").toString().trim();
 
 module.exports = {
   /**
@@ -90,6 +94,18 @@ module.exports = {
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
     //
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonicPhrase,
+          `https://sepolia.infura.io/v3/${infuraProjectId}`
+        ),
+      network_id: 11155111, // Sepolia's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+
     // Useful for private networks
     // private: {
     //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
@@ -121,6 +137,11 @@ module.exports = {
       //  evmVersion: "byzantium"
       // }
     },
+  },
+
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: etherScanKey,
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
