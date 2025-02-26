4cf95b60b7ceb42b7181911d99cd188eda8c62c9
Nathaniel Quansah <n.bongo40@gmail.com>
2025-02-26 14:37:53 +0000

    contract debugging

--- contracts/Spacebear.sol
+++ contracts/Spacebear.sol
@@ -2,50 +2,51 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.21;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import "@ganache/console.log/console.sol";
 
-contract Spacebear is ERC721, ERC721URIStorage, Ownable {
-    uint256 private _nextTokenId;
+contract Spacebear is ERC721, Ownable {
+  uint256 private _nextTokenId;
 
-    constructor(address initialOwner)
-        ERC721("Spacebear", "SBR")
-        Ownable(initialOwner)
-    {}
+  constructor(
+    address initialOwner
+  ) ERC721("Spacebear", "SBR") Ownable(initialOwner) {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
-    }
+  function _baseURI() internal pure override returns (string memory) {
+    return
+      "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
+  }
 
-    function safeMint(address to, string memory uri)
-        public
-        onlyOwner
-        returns (uint256)
-    {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        return tokenId;
-    }
+  function buyToken() public payable {
+    uint256 tokenId = _nextTokenId;
+    console.log("got here", msg.value);
+    require(
+      msg.value == (tokenId + 1) * 0.1 ether,
+      " Wrong amount of funds sent"
+    );
 
-    // The following functions are overrides required by Solidity.
+    _nextTokenId++;
+    _safeMint(msg.sender, tokenId);
+  }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+  function safeMint(address to) public onlyOwner {
+    uint256 tokenId = _nextTokenId++;
+    _safeMint(to, tokenId);
+  }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  // The following functions are overrides required by Solidity.
+
+  function tokenURI(
+    uint256 tokenId
+  ) public pure override(ERC721) returns (string memory) {
+    return
+      string(abi.encodePacked(_baseURI(), "spacebear_", tokenId + 1, ".json"));
+  }
+
+  function supportsInterface(
+    bytes4 interfaceId
+  ) public view override(ERC721) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -105,7 +105,17 @@ module.exports = {
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
-
+    somnia: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonicPhrase,
+          "https://dream-rpc.somnia.network",
+        ),
+      network_id: 50312, // Sepolia's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
